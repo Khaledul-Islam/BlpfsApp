987cc9882893fc4b88286c118134747e713d4267
k.islam <k.islam@blmanagedservices.com>
2024-03-04 17:18:23 +0600

    added axios in App

--- Screens/LoginScreen.jsx
+++ Screens/LoginScreen.jsx
@@ -1,19 +1,47 @@
 // LoginScreen.jsx
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
+import axios from 'axios';
 
-const LoginScreen = ({ navigation }) => {
+const LoginScreen = ({navigation}) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    // Simulate API call for login (replace this with your actual API call)
-    if (username === 'user' && password === 'password') {
-      // Navigate to the dashboard or homepage on successful login
-      navigation.navigate('Home');
-    } else {
-      // Handle unsuccessful login (show an error message or redirect to the login page)
-      alert('Invalid credentials. Please try again.');
+  const handleLogin = async () => {
+    try {
+      // Validate input fields
+      if (!username || !password) {
+        alert('Please enter both username and password.');
+        return;
+      }
+
+      // API endpoint and base URL
+      const apiUrl = 'user/ApiLogin';
+      const baseUrl = 'https://localhost:44309/';
+
+      // Make API call using Axios
+      const response = await axios.post(`${baseUrl}${apiUrl}`, {
+        Username: username,
+        Password: password,
+        RememberMe: true,
+        CheckoutAsGuest: false,
+        Email: 'k.islam@blmanagedservices.com',
+        UsernamesEnabled: true,
+        RegistrationType: 1,
+        DisplayCaptcha: false,
+      });
+
+      // Check the API response
+      if (response) {
+        // Navigate to the dashboard or homepage on successful login
+        navigation.navigate('Home');
+      } else {
+        // Handle unsuccessful login (show an error message)
+        alert('Invalid credentials. Please try again.');
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      alert('An error occurred during login. Please try again.');
     }
   };
 
@@ -24,14 +52,14 @@ const LoginScreen = ({ navigation }) => {
         style={styles.input}
         placeholder="Username"
         value={username}
-        onChangeText={(text) => setUsername(text)}
+        onChangeText={text => setUsername(text)}
       />
       <TextInput
         style={styles.input}
         placeholder="Password"
         secureTextEntry
         value={password}
-        onChangeText={(text) => setPassword(text)}
+        onChangeText={text => setPassword(text)}
       />
       <Button title="Login" onPress={handleLogin} />
     </View>
