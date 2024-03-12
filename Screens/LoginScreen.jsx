7d2812ae9fce9a80b6d8fedf2e2063236b30c068
k.islam <k.islam@blmanagedservices.com>
2024-03-12 12:04:07 +0600

    Added Login UI Design

--- Screens/LoginScreen.jsx
+++ Screens/LoginScreen.jsx
@@ -1,13 +1,21 @@
 // LoginScreen.jsx
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import axios from 'axios';
 import Loader from '../Components/Loader';
-import { useAuthDispatch } from '../Navigation/AuthContext';
+import {useAuthDispatch} from '../Navigation/AuthContext';
 import Toast from 'react-native-toast-message';
 import appConfig from '../app.json';
 
-const LoginScreen = ({ navigation }) => {
+const LoginScreen = ({navigation}) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
@@ -16,7 +24,7 @@ const LoginScreen = ({ navigation }) => {
   const handleLogin = async () => {
     try {
       const apiUrl = 'user/ApiLogin';
-      const baseUrl = appConfig.apiBaseURL; 
+      const baseUrl = appConfig.apiBaseURL;
       setLoading(true);
       const response = await axios.post(`${baseUrl}${apiUrl}`, {
         email: 'admin@pfs.com',
@@ -30,17 +38,17 @@ const LoginScreen = ({ navigation }) => {
         navigation.navigate('Home');
       } else {
         Toast.show({
-          type:'error',
-          text1:'Error',
-          text2:'Invalid credentials. Please try again.',        
-        })
+          type: 'error',
+          text1: 'Error',
+          text2: 'Invalid credentials. Please try again.',
+        });
       }
     } catch (error) {
       Toast.show({
-        type:'error',
-        text1:'Error',
-        text2:'An error occurred during login. Please try again.',        
-      })
+        type: 'error',
+        text1: 'Error',
+        text2: 'An error occurred during login. Please try again.',
+      });
     } finally {
       setLoading(false);
     }
@@ -48,31 +56,41 @@ const LoginScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={(text) => setUsername(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
-      <Button title="Login" onPress={handleLogin} />
+      <Image style={styles.image} source={require("../Asset/Login.png")} />
+      {/* <StatusBar style="auto" /> */}
+      <Text style={styles.title}>User Login</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email"
+          value={username}
+          placeholderTextColor="#003f5c"
+          onChangeText={text => setUsername(text)}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          value={password}
+          secureTextEntry={true}
+          onChangeText={text => setPassword(text)}
+        />
+      </View>
+      <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
       {loading && <Loader loading={loading} />}
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     fontSize: 24,
@@ -80,14 +98,32 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     color: 'black',
   },
-  input: {
-    height: 40,
-    width: 250,
-    borderColor: 'gray',
-    borderWidth: 1,
+  image: {
+    marginBottom: 40,
+  },
+  inputView: {
+    backgroundColor: '#FAF9F9',
+    borderRadius: 30,
+    borderColor: '#c9bfbf',
+    borderWidth: 2,
+    width: '70%',
+    height: 45,
     marginBottom: 20,
-    paddingLeft: 10,
-    color: 'black',
+    alignItems: 'center',
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  loginBtn: {
+    width: '40%',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#4ab2f1',
   },
 });
 
